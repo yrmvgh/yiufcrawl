10df492f0801ef9cb79016ea116df9532f406eb1
Steve Melenchuk <smelenchuk@gmail.com>
2014-12-04 09:49:56 -0700

    Make monster Gravitas marginally more intelligent.
    It's useless if there's nothing near the target that can move.

--- crawl-ref/source/mon-cast.cc
+++ crawl-ref/source/mon-cast.cc
@@ -8044,6 +8044,19 @@ static bool _ms_waste_of_time(monster* mon, mon_spell_slot slot)
         return !mons_should_fire(tracer);
     }
 
+    case SPELL_GRAVITAS:
+        if (!foe)
+            return true;
+
+        for (actor_near_iterator ai(foe); ai; ++ai)
+            if (*ai != mon && *ai != foe && !ai->is_stationary()
+                && mon->can_see(*ai))
+            {
+                return false;
+            }
+
+        return true;
+
 #if TAG_MAJOR_VERSION == 34
     case SPELL_SUMMON_TWISTER:
     case SPELL_SHAFT_SELF:
