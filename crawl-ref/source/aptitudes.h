64239cecccca53e47961ff5a9a9c63fad96a30a9
jeremy <jeremy.gurr@gmail.com>
2016-02-12 05:15:49 -0700

    adjusted deep elf and high elf

--- crawl-ref/source/aptitudes.h
+++ crawl-ref/source/aptitudes.h
@@ -113,7 +113,7 @@ static const species_skill_aptitude species_skill_aptitudes[] =
     APT(SP_DEEP_ELF,        SK_TRAPS,         UNUSABLE_SKILL),
 #endif
     APT(SP_DEEP_ELF,        SK_UNARMED_COMBAT, -2),
-    APT(SP_DEEP_ELF,        SK_SPELLCASTING,    3),
+    APT(SP_DEEP_ELF,        SK_SPELLCASTING,    6),
     APT(SP_DEEP_ELF,        SK_CONJURATIONS,    1),
     APT(SP_DEEP_ELF,        SK_HEXES,           3),
     APT(SP_DEEP_ELF,        SK_CHARMS,          4),
