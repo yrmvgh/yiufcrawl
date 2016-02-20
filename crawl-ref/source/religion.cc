aadc8fc88ff138dba59d4bfa9cb7c1f6cdbee34a
Corin Buchanan-Howland <corin@buchananhowland.net>
2016-02-20 16:55:52 -0500

    Add Ukayaw wrath
    Right now the wrath can try to berserk monsters near you, or paralyse you
    with monsters nearby, or make a really loud noise. There's room to get
    more creative here, but I just wanted to put in something adequate for now
    so that this god is more trunk-ready.

--- crawl-ref/source/religion.cc
+++ crawl-ref/source/religion.cc
@@ -1059,7 +1059,6 @@ bool active_penance(god_type god)
            && god != GOD_ASHENZARI
            && god != GOD_GOZAG
            && god != GOD_RU
-           && god != GOD_UKAYAW
            && (god != GOD_NEMELEX_XOBEH || you.penance[god] > 100)
            && (god == you.religion && !is_good_god(god)
                || god_hates_your_god(god, you.religion));
