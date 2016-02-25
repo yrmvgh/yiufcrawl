56f90cd6842149c5dd52ef50598e2d299d864755
jeremy <jeremy.gurr@gmail.com>
2016-02-24 21:39:40 -0700

    made Dj take into account mana regen properly

--- crawl-ref/source/player-reacts.cc
+++ crawl-ref/source/player-reacts.cc
@@ -852,7 +852,7 @@ static void _regenerate_hp_and_mp(int delay)
     if (!player_regenerates_mp())
         return;
 
-    if (you.magic_points < you.max_magic_points)
+    if (you.magic_points < you.max_magic_points || you.species == SP_DJINNI && you.hp < you.hp_max)
     {
         const int base_val = 7 + you.max_magic_points / 2;
         int mp_regen_countup = div_rand_round(base_val * delay, BASELINE_DELAY);
