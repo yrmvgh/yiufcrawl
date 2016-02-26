9b30bf8bb25ef1ae196d5415a8a02743f0bf5e67
jeremy <jeremy.gurr@gmail.com>
2016-02-25 19:30:04 -0700

    revert los change so it will work with web tiles

--- crawl-ref/source/defines.h
+++ crawl-ref/source/defines.h
@@ -100,7 +100,7 @@ const int LABYRINTH_BORDER = 4;
 #define Y_WIDTH                 (Y_BOUND_2 - Y_BOUND_1 + 1)
 
 // normal LOS radius
-#define LOS_RADIUS 9
+#define LOS_RADIUS 7
 
 // maximal horizontal or vertical LOS range:
 //   a quadrant needs to fit inside an 2D array with
