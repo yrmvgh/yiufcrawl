fcb0bf25ba68a50634fc70f1f748bcb7207f1851
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-03-19 07:43:55 -0700

    lowered lignify health gain a little

--- crawl-ref/source/form-data.h
+++ crawl-ref/source/form-data.h
@@ -165,7 +165,7 @@ static const form_entry formdata[] =
     "a tree.",
     EQF_LEAR | SLOTF(EQ_CLOAK), MR_RES_POISON | mrd(MR_RES_NEG, 3),
     BAD_DURATION, 0, 0, SIZE_CHARACTER, 20, 27,
-    15, 0, 150, true, 0, 10, 12,
+    15, 0, 100, true, 0, 10, 12,
     SPWPN_NORMAL, BROWN, "Branches", { "hit", "smack", "pummel", "thrash" },
     FC_FORBID, FC_FORBID, FC_FORBID, false, false,
     "creak", 0, "branch", "root", "sway towards", "wood"
