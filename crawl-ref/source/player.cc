d03ca27c20bc28129f0379c2099de4cc1cfeb5d0
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-04-05 13:45:03 -0700

    don't give experience for safe floors (temple, trove, bazaar)

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -2678,6 +2678,10 @@ static void _reduce_abyss_xp_timer(int exp)
 const int experience_for_this_floor() {
     const int how_deep = absdungeon_depth(you.where_are_you, you.depth);
     int exp = stepup2(how_deep + 1, 3, 3, 10) + 1;
+    if (you.where_are_you == BRANCH_BAZAAR
+        || you.where_are_you == BRANCH_TEMPLE
+        || you.where_are_you == BRANCH_TROVE)
+        exp = 0;
     return exp;
 }
 
