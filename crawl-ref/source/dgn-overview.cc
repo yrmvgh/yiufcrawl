11142180e1e9625d81013dd1dd94d4eec6c74565
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-04-06 08:33:07 -0700

    another attempt to fix the annotations issue

--- crawl-ref/source/dgn-overview.cc
+++ crawl-ref/source/dgn-overview.cc
@@ -894,17 +894,8 @@ void add_experience_potion_annotation(level_id li, int count)
         potions = *map_find(level_experience, li);
     }
     potions += count;
-//    for (const item_def &item : mitm)
-//    {
-//        if(item.base_type == OBJ_POTIONS
-//           && item.sub_type == POT_EXPERIENCE
-//           && item.orig_place.branch == li.branch
-//           && item.orig_place.depth == li.depth)
-//            potions++;
-//    }
-//
-//    if (count > 0)
-//        potions += count;
+    if (count < 0)
+        count = 0;
 
     level_experience[li] = potions;
 }
