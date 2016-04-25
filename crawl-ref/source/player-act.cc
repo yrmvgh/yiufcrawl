4e0311f8a4bf1d5b48bbd9546c96972686e0dc5c
Jeremy Gurr <jeremy.gurr@gmail.com>
2016-04-24 23:04:14 -0700

    berserk doesn't force power mode, and potion of berserk can be used even when tired

--- crawl-ref/source/player-act.cc
+++ crawl-ref/source/player-act.cc
@@ -752,13 +752,8 @@ bool player::go_berserk(bool intentional, bool potion)
     if (!you.can_go_berserk(intentional, potion))
         return false;
 
-    if (player_is_tired())
-        return false;
-
     if (check_stasis())
         return false;
-
-    set_exertion(EXERT_POWER);
 
     if (crawl_state.game_is_hints())
         Hints.hints_berserk_counter++;
