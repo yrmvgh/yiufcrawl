6010986615d5fc6d3e4b388d04f1470e0b5deae4
Nicholas Feinberg <pleasingfung@gmail.com>
2017-01-14 11:22:47 -0800

    Turn Bai Suizhen's thunder into an ench (10730)
    So that she stops murdering everyone on the level if you leave and
    come back from another set of stairs.

--- crawl-ref/source/enum.h
+++ crawl-ref/source/enum.h
@@ -2102,6 +2102,7 @@ enum enchant_type
     ENCH_BOUND_SOUL,
     ENCH_INFESTATION,
     ENCH_STILL_WINDS,
+    ENCH_RING_OF_THUNDER,
     // Update enchant_names[] in mon-ench.cc when adding or removing
     // enchantments.
     NUM_ENCHANTMENTS
@@ -4758,6 +4759,7 @@ enum spell_type : int
     SPELL_DREAM_DUST,
     SPELL_BECKONING,
     SPELL_UPHEAVAL,
+    SPELL_RING_OF_THUNDER,
     NUM_SPELLS
 };
 
