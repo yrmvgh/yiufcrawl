ecbb7a5b863e47aec7a8f142ff9b106ab0ea79b1
Chris Campbell <chriscampbell89@gmail.com>
2017-01-20 23:30:35 +0000

    Fix amulet of reflection pluses not changing their shop price

--- crawl-ref/source/shopping.cc
+++ crawl-ref/source/shopping.cc
@@ -625,7 +625,8 @@ unsigned int item_value(item_def item, bool ident)
                     || item.sub_type == RING_EVASION
                     || item.sub_type == RING_DEXTERITY
                     || item.sub_type == RING_INTELLIGENCE
-                    || item.sub_type == RING_SLAYING))
+                    || item.sub_type == RING_SLAYING
+                    || item.sub_type == AMU_REFLECTION))
             {
                 // Formula: price = kn(n+1) / 2, where k depends on the subtype,
                 // n is the power. (The base variable is equal to 2n.)
@@ -646,6 +647,7 @@ unsigned int item_value(item_def item, bool ident)
                 case RING_STRENGTH:
                 case RING_DEXTERITY:
                 case RING_INTELLIGENCE:
+                case AMU_REFLECTION:
                     coefficient = 30;
                     break;
                 default:
@@ -676,7 +678,6 @@ unsigned int item_value(item_def item, bool ident)
                 case RING_FIRE:
                 case RING_ICE:
                 case RING_PROTECTION_FROM_MAGIC:
-                case AMU_REFLECTION:
                     valued += 250;
                     break;
 
