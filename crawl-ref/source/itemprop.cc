7d47d568860251ebe05f9812df0b7c13355dad03
Yer mivvaggah <newtn.rh@gmail.com>
2017-02-20 01:08:37 -0500

    mark needles as removed

--- crawl-ref/source/itemprop.cc
+++ crawl-ref/source/itemprop.cc
@@ -805,6 +805,7 @@ const set<pair<object_class_type, int> > removed_items =
 	{ OBJ_SCROLLS,   SCR_IDENTIFY },
 	{ OBJ_SCROLLS,   SCR_REMOVE_CURSE },
 	{ OBJ_SCROLLS,   SCR_RANDOM_USELESSNESS },
+	{ OBJ_MISSILES,   MI_NEEDLE },
 #endif
     // Outside the #if because we probably won't remove these.
     { OBJ_RUNES,     RUNE_ELF },
