aeb14320c066962045acaf92db6548122bb564e3
yrmvgh <yrmvgh@users.noreply.github.com>
2017-03-19 03:04:42 -0400

    bearserk ability

--- crawl-ref/source/duration-data.h
+++ crawl-ref/source/duration-data.h
@@ -546,12 +546,7 @@ static const duration_def duration_data[] =
       "", D_NO_FLAGS,
       {{ "You are ready to hop once more." }}},
     { DUR_CORNERED, YELLOW, "Corner", "", "cornered",
-      "You feel cornered and can go bearserk.", D_NO_FLAGS },
-    { DUR_HEAVEN_ON_EARTH,
-      0, "", "", "", "", D_NO_FLAGS,
-      {{ "", []() {
-          wu_jian_heaven_tick();
-      }}}},
+      "You feel cornered and ready to bearserk.", D_NO_FLAGS },
 
     // The following are visible in wizmode only, or are handled
     // specially in the status lights and/or the % or @ screens.
