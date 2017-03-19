b2c6101322983420a9d932df54a7b10f1457e307
yrmvgh <yrmvgh@users.noreply.github.com>
2017-03-19 04:54:28 -0400

    felids get 9 lives immediately, no gain

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -568,6 +568,9 @@ static void _setup_generic(const newgame_def& ng)
     // Get rid of god companions left from previous games
     init_companions();
 
+    if (you.species == SP_FELID)
+        you.lives = 9;
+
     // Create the save file.
     if (Options.no_save)
         you.save = new package();
