ad642354578d70a63022dce6448cec2a8810ef02
yrmvgh <yrmvgh@users.noreply.github.com>
2017-03-29 17:08:27 -0400

    improve newgame menu
    increase offset between special/normal species, improve bk descript

--- crawl-ref/source/newgame.cc
+++ crawl-ref/source/newgame.cc
@@ -954,7 +954,7 @@ static species_group species_groups[] =
     },
     {
         "DCSS Species; (YIUF)",
-        coord_def(20, -1),
+        coord_def(22, -1),
         20,
         {
             SP_BARACHIAN,
@@ -974,7 +974,7 @@ static species_group species_groups[] =
     },
     {
         " ones are altered.",
-        coord_def(40, -1),
+        coord_def(42, -1),
         20,
         {
             SP_HUMAN,
