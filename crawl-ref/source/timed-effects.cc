a0142211b826d2af9da7855f9592581d0ab814c8
advil <rawlins@gmail.com>
2017-04-08 23:41:58 -0400

    Fix a deletion case, add some docstrings
    Fix a regression in this branch, where deleting a mutation could fail to
    delete a normal mutation if there were temporary mutations.  Also,
    remove an unused parameter for `mutate()`.

--- crawl-ref/source/timed-effects.cc
+++ crawl-ref/source/timed-effects.cc
@@ -788,7 +788,7 @@ static void _magic_contamination_effects()
     if (you.species != SP_PLUTONIAN)
     {
         mutate(one_chance_in(5) ? RANDOM_MUTATION : RANDOM_BAD_MUTATION,
-           "mutagenic glow", true, coinflip(), false, false, mutclass, false);
+               "mutagenic glow", true, coinflip(), false, false, mutclass);
     }
 
     // we're meaner now, what with explosions and whatnot, but
@@ -913,7 +913,7 @@ static void _evolve(int time_delta)
             {
                 mpr("You feel a VERY uncomfortable genetic drift.");
                 mutate(RANDOM_SLIME_MUTATION, "evolution", false,
-                       false, false, false, MUTCLASS_INNATE, true);
+                       false, false, false, MUTCLASS_INNATE);
                 more();
                 const string reason = (you.get_mutation_level(MUT_EVOLUTION) == 1)
                                     ? "end of evolution"
@@ -926,8 +926,7 @@ static void _evolve(int time_delta)
                 bool evol = one_chance_in(5) ?
                     delete_mutation(RANDOM_BAD_MUTATION, "evolution", false) :
                     mutate(random_choose(RANDOM_GOOD_MUTATION, RANDOM_MUTATION),
-                           "evolution", false, false, false, false, MUTCLASS_NORMAL,
-                           true);
+                           "evolution", false, false, false, false, MUTCLASS_NORMAL,);
                 // it would kill itself anyway, but let's speed that up
                 if (one_chance_in(10)
                     && (!you.rmut_from_item()
